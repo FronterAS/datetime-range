a92be4d4a77f38f2c6e0fa003319c6275aebcc45
Gavin King <gavin.king@fronter.com>
2014-03-14 01:40:58 +0100

    Breaking it out a little more, using convenience functions and general breaking out of the code

---
+++ app/directives.js
@@ -0,0 +1,297 @@
+'use strict';
+
+angular.module('UIcomponents')
+    .directive(
+        'datetimeRange',
+        function datetimeRangeDirective($timeout, DatetimeRange) {
+            var scope,
+                element,
+
+                datetimeRange = new DatetimeRange(),
+
+                updateData = function () {
+                    var data = datetimeRange.getData();
+
+                    element.data('datetimeRange', data);
+                    console.log(data);
+                },
+
+                onAllDayChange = function (e, isChecked) {
+                    // We are using allDay to hide and show the time directives.
+                    scope.allDay = isChecked;
+
+                    datetimeRange.setAllDay(scope.allDay);
+                    updateData();
+
+                    // Let the children know
+                    scope.$broadcast('allDayChanged', scope.allDay);
+
+
+                    // our ng-if badly needs to know about this
+                    scope.$apply();
+                },
+
+                onStartDateChange = function (e, date) {
+                    datetimeRange.setStartDate(date);
+                    updateData();
+
+                    scope.$broadcast('startDateChanged', date);
+                },
+
+                onEndDateChange = function (e, date) {
+                    datetimeRange.setEndDate(date);
+                    updateData();
+
+                    scope.$broadcast('endDateChanged', date);
+                },
+
+                onStartTimeChange = function (e, time) {
+                    datetimeRange.setStartTime(time);
+                    updateData();
+
+                    scope.$broadcast('startTimeChanged', time);
+                },
+
+                onEndTimeChange = function (e, time) {
+                    datetimeRange.setEndTime(time);
+                    scope.$broadcast('endTimeChanged', time);
+                },
+
+                isSameDay = function () {
+                    return datetimeRange.startDate === datetimeRange.endDate;
+                };
+
+            return {
+                restrict: 'E',
+                scope: {
+                    dateFormat: '@',
+                    timeFormat: '@'
+                },
+
+                link: function (_scope_, _element_) {
+                    scope = _scope_;
+                    element = _element_;
+
+                    scope.isSameDay = isSameDay;
+
+                    // much topic, so callback, wow
+                    scope.$on('allDayChange', onAllDayChange);
+                    scope.$on('startDateChange', onStartDateChange);
+                    scope.$on('endDateChange', onEndDateChange);
+                    scope.$on('startTimeChange', onStartTimeChange);
+                    scope.$on('endTimeChange', onEndTimeChange);
+                },
+                templateUrl: 'datetimeRange.html'
+            };
+        }
+    )
+    .directive(
+        'datePicker',
+        /**
+         * Sets up an element as a datepicker.
+         *
+         * @return {object}
+         */
+        function datePickerDirective() {
+            return {
+                restrict: 'E',
+                priority: 0,
+                replace: true,
+                scope: true,
+                link: function (scope, element, attrs) {
+                    element.pickadate({
+                        'format': scope.dateFormat,
+                        'formatSubmit': scope.dateFormat
+                    });
+                },
+                templateUrl: 'datepicker.html'
+            };
+        }
+    )
+    .directive(
+        'startDate',
+        /**
+         * Handles behaviour specific to a start date in a range directive.
+         *
+         * @return {object}
+         */
+        function startDatePickerDirective($timeout, DatetimeHelper) {
+            return {
+                restrict: 'A',
+                priority: 1,
+                link: function (scope, element, attrs) {
+                    var api = element.pickadate('picker');
+
+                    scope.$on('endDateChanged', function (e, date) {
+                        console.info('heard end date change, updating start date max');
+                        console.info(date);
+                        api.set('max', date, { format: scope.dateFormat });
+                    });
+
+                    $timeout(function () {
+                        // this will throw a change event
+                        if (!api.get('select')) {
+                            api.set('select', DatetimeHelper.getDate(scope.dateFormat));
+                        }
+                    }, 0);
+
+                    element.on('change', function () {
+                        scope.$emit('startDateChange', api.get('value'));
+                    });
+                }
+            };
+        }
+    )
+    .directive(
+        'endDate',
+        /**
+         * Handles behaviour specific to an end date in a range directive.
+         *
+         * @return {object}
+         */
+        function endDatePickerDirective($timeout, DatetimeHelper) {
+            return {
+                restrict: 'A',
+                priority: 2,
+                link: function (scope, element, attrs) {
+                    var api = element.pickadate('picker');
+
+                    scope.$on('startDateChanged', function (e, date) {
+                        console.info('heard start date change, updating end date min');
+                        console.info(date);
+                        api.set('min', date, { format: scope.dateFormat });
+                    });
+
+                    $timeout(function () {
+                        // this will throw a change event
+                        if (!api.get('select')) {
+                            api.set('select', DatetimeHelper.getDate(scope.dateFormat));
+                        }
+                    }, 0);
+
+                    element.on('change', function () {
+                        scope.$emit('endDateChange', api.get('value'));
+                    });
+                }
+            };
+        }
+    )
+    .directive(
+        'timePicker',
+        /**
+         * Sets up an element as a timepicker.
+         *
+         * @return {object}
+         */
+        function timePickerDirective() {
+            return {
+                restrict: 'E',
+                priority: 3,
+                replace: true,
+                scope: true,
+                link: function (scope, element, attrs) {
+                    element.pickatime({
+                        format: scope.timeFormat.replace('mm', 'i')
+                    });
+
+                    scope.$on('allDayChanged', function (e, isChecked) {
+                        if (!isChecked) {
+                            element.trigger('change');
+                        }
+                    });
+                },
+                templateUrl: 'timepicker.html'
+            };
+        }
+    )
+    .directive(
+        'startTime',
+        /**
+         * Handles behaviour specific to an start time in a range directive.
+         *
+         * @return {object}
+         */
+        function startTimePickerDirective($timeout, DatetimeHelper) {
+            return {
+                restrict: 'A',
+                priority: 4,
+                link: function (scope, element, attrs) {
+                    var api = element.pickatime('picker');
+
+                    $timeout(function () {
+                        // this will throw a change event
+                        if (!api.get('select')) {
+                            api.set('select', DatetimeHelper.getTime(scope.timeFormat));
+                        }
+                    }, 0);
+
+                    scope.$on('endTimeChanged', function (e, time) {
+                        if (scope.isSameDay()) {
+                            api.set('max', time);
+                        }
+                    });
+
+                    element.on('change', function () {
+                        scope.$emit('startTimeChange', api.get('value'));
+                    });
+                }
+            };
+        }
+    )
+    .directive(
+        'endTime',
+        /**
+         * Handles behaviour specific to an end time in a range directive.
+         *
+         * @return {object}
+         */
+        function endTimePickerDirective($timeout, DatetimeHelper) {
+            return {
+                restrict: 'A',
+                priority: 5,
+                link: function (scope, element, attrs) {
+                    var api = element.pickatime('picker');
+
+                    $timeout(function () {
+                        // this will throw a change event
+                        if (!api.get('select')) {
+                            api.set('select', DatetimeHelper.getTime(scope.timeFormat, 1));
+                        }
+                    }, 0);
+
+                    scope.$on('startTimeChanged', function (e, time) {
+                        if (scope.isSameDay()) {
+                            api.set('min', time);
+                        }
+                    });
+
+                    element.on('change', function () {
+                        scope.$emit('endTimeChange', api.get('value'));
+                    });
+                }
+            };
+        }
+    )
+    .directive(
+        'allday',
+        /**
+         * This directive shares the scope of the parent directive.
+         * @return {object}
+         */
+        function allDayDirective() {
+            return {
+                restrict: 'E',
+                replace: true,
+                scope: true,
+                link: function (scope, element, attrs) {
+                    // element is a checkbox
+                    element.on('change', function () {
+                        // We $emit the event up to the parent, so that the event
+                        // can be detected if wanted on all parent controllers.
+                        scope.$emit('allDayChange', element.is(':checked'));
+                    });
+                },
+                templateUrl: 'allday.html'
+            };
+        }
+    );
--- app/extras.js
+++ app/extras.js
@@ -1,9 +1,12 @@
+'use strict';
+
 angular.module('UIcomponents')
-    .service('DatetimeHelper', function () {
+    .service('DatetimeHelper', function DatetimeHelper() {
         return {
             /**
              * Wraps momentjs startOf('day')
              *
+             * @public
              * @param  {Date} date Can be any valid date that momentjs accepts.
              * @return {Date}      In ISOString format.
              */
@@ -14,34 +17,49 @@ angular.module('UIcomponents')
             /**
              * Wraps momentjs endOf('day')
              *
-             * @param  {Date} date Can be any valid date that momentjs accepts.
-             * @return {Date}      In ISOString format.
+             * @public
+             * @param  {string} date Can be any valid date that momentjs accepts.
+             * @return {string}      In ISOString format.
              */
             getEndOfDay: function (date) {
                 return moment(date).endOf('day').toISOString();
             },
 
             /**
-             * Updates the time on an ISOString date.
+             * Updates the time on any date that moment accepts.
              *
-             * @return {Date} ISOString format
+             * @public
+             * @param {string} date Any date that moment accepts
+             * @return {string} ISOString format
              */
-            updateTime: function (date, time) {
-                var m = moment(date);
+            create: function (date, time) {
+                var m = moment(date),
+                    parts;
 
-                m.hours(time.hour);
-                m.minutes(time.mins);
+                if (time) {
+                    parts = time.split(':');
+                    m.hours(parseInt(parts[0], 10));
+                    m.minutes(parseInt(parts[1], 10));
+                }
 
                 return m.toISOString();
             },
 
+            /**
+             * @public
+             * @param {formatString} formatString A valid date format string.
+             */
             getDate: function (formatString) {
                 return moment().format(formatString);
             },
 
+            /**
+             * @public
+             * @param {string} formatString A valid time format string.
+             *                              Defaults to 'HH:mm'
+             */
             getTime: function (formatString, hours, minutes) {
                 var m = moment();
-
 
                 formatString = formatString || 'HH:mm';
 
@@ -57,45 +75,79 @@ angular.module('UIcomponents')
             }
         };
     })
-    .factory('DatetimeRange', function (DatetimeHelper) {
-        return function DatetimeRangeFactory() {
+    .factory('DatetimeRange', function DatetimeRangeFactory(DatetimeHelper) {
+        return function DatetimeRange() {
             var data = {
                     'startDate': null,
                     'endDate'  : null,
                     'allDay'   : false
                 },
 
-                toArray = function (date) {
-                    return moment(date).toArray();
+                startTimeCache,
+                endTimeCache,
+
+                /**
+                 * Sets start or end date with ISOString.
+                 *
+                 * @param {string} key The data key to update. Can be 'start' or 'end'.
+                 * @param {string} date Any date format that moment accepts.
+                 * @param {object} time The time object from pickadate.
+                 * @private
+                 */
+                setDateTime = function (key, date, time) {
+                    var datetime = DatetimeHelper.create(date, time);
+
+                    data[key + 'Date'] = datetime;
+                    return this;
                 },
 
+                /**
+                 * Convenience to ensure the time is set alongside the date.
+                 *
+                 * @public
+                 */
                 setStartDate = function (date) {
-                    var parts = toArray(date);
-
-                    // parts
+                    setDateTime('start', date, startTimeCache);
 
                     console.info('changed start date to:');
                     console.info(date);
                     return this;
                 },
 
+                /**
+                 * Convenience to ensure the time is set alongside the date.
+                 *
+                 * @public
+                 */
                 setEndDate = function (date) {
+                    setDateTime('end', date, endTimeCache);
+
                     console.info('changed end date to:');
                     console.info(date);
                     return this;
                 },
 
+                /**
+                 * @public
+                 */
                 setStartTime = function (time) {
+                    startTimeCache = time;
+                    data.startDate = DatetimeHelper.create(data.startDate, time);
+
                     console.info('changed start time to:');
-                    console.info(time);
-                    data.startDate = DatetimeHelper.updateTime(data.startDate, time);
+                    console.info(data.startDate);
                     return this;
                 },
 
+                /**
+                 * @public
+                 */
                 setEndTime = function (time) {
+                    endTimeCache = time;
+                    data.endDate = DatetimeHelper.create(data.endDate, time);
+
                     console.info('changed end time to:');
-                    console.info(time);
-                    data.endDate = DatetimeHelper.updateTime(data.endDate, time);
+                    console.info(data.endDate);
                     return this;
                 },
 
@@ -106,17 +158,25 @@ angular.module('UIcomponents')
                  * @todo What happens if something is not all day? Should we cache the previous
                  *       times and replace them into the date strings?
                  *
+                 * @public
                  * @param {Boolean} isAllDay
                  */
                 setAllDay = function (isAllDay) {
                     console.info('changed all day to:');
-                    console.log(isChecked);
+                    console.log(isAllDay);
 
                     if (isAllDay) {
                         // startDate and endDate always exist
-                        data.startDate = DatetimeHelper.getStartOfDay(data.startDate);
-                        data.endDate = DatetimeHelper.getEndOfDay(data.startDate);
+                        setDateTime('start', DatetimeHelper.getStartOfDay(data.startDate));
+                        setDateTime('end', DatetimeHelper.getEndOfDay(data.startDate));
                     }
+                },
+
+                /**
+                 * @public
+                 */
+                getData = function () {
+                    return data;
                 };
 
             // API
@@ -125,7 +185,8 @@ angular.module('UIcomponents')
                 'setEndDate': setEndDate,
                 'setStartTime': setStartTime,
                 'setEndTime': setEndTime,
-                'setAllDay': setAllDay
+                'setAllDay': setAllDay,
+                'getData': getData
             };
         };
     });
